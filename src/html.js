383c5339d2ed7b992d2c8631c5688e89182e7694
Camilo Contreras <camilo.coo250@gmail.com>
2019-05-28 20:35:01 +0000

    title added to the html head

--- src/html.js
+++ src/html.js
@@ -6,7 +6,7 @@ export default function HTML(props) {
     <html {...props.htmlAttributes}>
       <head>
         <meta charSet="utf-8" />
-
+         <title>BreatheCode</title>
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta
           name="viewport"
