6c2dc0a36f4cc972987e246b14925e6b71315337
Camilo Contreras <camilo.coo250@gmail.com>
2019-05-28 17:47:02 +0000

    site metadata added and icon added to the page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `BreatheCode`,
+    description: `BreatheCode's mission is to accelerate the way junior developers learn and evolve using technology.`,
     author: `@gatsbyjs`,
   },
   plugins: [
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/breathecode.32.png`, // This path is relative to the root of the site.
       },
     },
    {
