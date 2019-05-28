11b1771f51314136abef42c7ee62ea758e6a28b1
Camilo Contreras <camilo.coo250@gmail.com>
2019-05-28 17:28:13 +0000

    gtm installed and setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,21 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+   {
+    resolve: `gatsby-plugin-google-tagmanager`,
+    options: {
+      id: "GTM-574Z6C5",
+
+      // Include GTM in development.
+      // Defaults to false meaning GTM will only be loaded in production.
+      includeInDevelopment: false,
+
+      // Specify optional GTM environment details.
+      gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIROMENT_AUTH_STRING",
+      gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIROMENT_PREVIEW_NAME",
+      dataLayerName: "YOUR_DATA_LAYER_NAME",
+    },
+  },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
