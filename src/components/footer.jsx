3a4b59281f888fd2449f2e2fac56e7f909fa509b
CSAV <calisayaariel62@gmail.com>
2021-03-11 17:55:05 -0300

    changed href link

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -19,7 +19,7 @@ export class Footer extends Component {
 								{` `}
 								in collaboration with
 								{` `}
-								<a href="https://www.breatheco.de">4geeks Academy</a>
+								<a href="https://4geeksacademy.com/">4geeks Academy</a>
 
 							</div>
 						</div>
